9181262ed234ffd610e6999eabbf27df2d9afa70
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-05-09 08:44:32 +0300

    style: benefits card drop shadow, border radius, subtitle sizing

--- src/components/landingPage/benefits/BenefitsCard.tsx
+++ src/components/landingPage/benefits/BenefitsCard.tsx
@@ -16,16 +16,22 @@ function BenefitsCard({
           p: 1,
           m: 2,
           height: "40vh",
+          borderRadius: "26px",
+          boxShadow: "10px 10px 10px black",
           width: { xs: "100%", md: "30%" },
         }}
       >
-        <Box sx={{ height: "50%", pt: 5, textAlign: "center" }}>
+        <Box sx={{ height: "50%", pt: 5, textAlign: "center", pl: 1, pr: 1 }}>
           <Typography variant="h4" component={"h3"}>
             {title}
           </Typography>
         </Box>
-        <Box sx={{ height: "50%", textAlign: "center" }}>
-          <Typography variant="subtitle1" component={"h5"}>
+        <Box sx={{ height: "50%", textAlign: "center", p: 1 }}>
+          <Typography
+            variant="subtitle1"
+            component={"h5"}
+            sx={{ fontSize: "1.5rem" }}
+          >
             {description}
           </Typography>
         </Box>
