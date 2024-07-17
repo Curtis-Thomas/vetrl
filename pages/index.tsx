fdc988f1fd1ff23abb1f86a92b55b0aac2e65928
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-07-17 16:00:34 +0300

    add: favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,24 @@ const Home: React.FC<HomeProps> = () => {
           name="description"
           content="Online Veterinary client management."
         />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
       </Head>
       <Box
         component="header"
